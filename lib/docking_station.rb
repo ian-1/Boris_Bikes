702f80cb0864e849b0837624403952c637169248
ian-1 <ian.walton@portmoor.com>
2021-03-31 15:25:23 +0100

    add attr reader bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,11 @@
 class DockingStation
+  attr_reader :bike
+  
   def release_bike
     Bike.new
   end
 
-  def dock_bike
-
+  def dock_bike(bike)
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,24 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'releases bike' do
-    expect(DockingStation.new).to respond_to(:release_bike)
-  end
+  it { is_expected.to respond_to :release_bike }
 
   it 'gets bike' do
-    expect(DockingStation.new.release_bike).to be_instance_of(Bike)
+    expect(subject.release_bike).to be_instance_of(Bike)
   end
 
-  it 'docks bike' do
-    expect(DockingStation.new).to respond_to(:dock_bike) 
-  end
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+  it { is_expected.to respond_to(:bike)}
+
+ it 'check dock_bike(bike) returns bike)' do
+   bike = Bike.new
+   expect(subject.dock_bike(bike)).to eq(bike)
+ end
+
+ it '' do
+  bike = Bike.new
+  subject.dock_bike(bike)
+  expect(subject.bike).to eq(bike)
+ end
 end
